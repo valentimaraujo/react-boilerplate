f5d8449254d02aaed0bec5ff53f541d9e564cda6
Valentim Araújo <valentim@evodev.com.br>
2022-06-11 19:08:15 -0300

    add jest

--- src/pages/index.spec.ts
+++ src/pages/index.spec.ts
@@ -1,8 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 describe('Apenas configurando jest', () => {
-  it('Fornçando o erro sdfs', () => {
+  it('Fornçando o erro', () => {
     expect(true).toBeTruthy();
-    expect(false).toBeTruthy();
   });
 });
