79ed4203e0596b99c8597a499cbc85c304dbb427
Valentim Araújo <valentim@evodev.com.br>
2022-06-11 20:46:57 -0300

    config Global Styles

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import GlobalStyles from 'style/global';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado</title>
+        <link rel="shortcut icon" href="/vercel.svg" />
+        <link rel="apple-touch-icon" href="/vercel.svg" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default App;
