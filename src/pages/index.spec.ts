55f3be01ff2938940dde57baf7f65236a0f0e358
Valentim Araújo <valentim@evodev.com.br>
2022-06-11 18:38:04 -0300

    add jest

---
+++ src/pages/index.spec.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+describe('Apenas configurando jest', () => {
+  it('Fornçando o erro', () => {
+    expect(false).toBeTruthy();
+  });
+});
