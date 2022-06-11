a4db94300fa176c9660b66a9d3d121ac92bf866e
Valentim Araújo <valentim@evodev.com.br>
2022-06-11 18:29:40 -0300

    add jest

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "**/*.ts?(x)": (filenames) =>
+  "./src/**/*.ts?(x)": (filenames) =>
     `next lint --max-warnings=0 --fix --file ${filenames
       .map((file) => file.split(process.cwd())[1])
       .join(" --file ")}`,
