09e83bbc721abe35b2e5868a96412cb371548c45
Valentim Araújo <valentim@evodev.com.br>
2022-07-14 10:06:30 -0300

    fix eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,17 @@ import Head from 'next/head';
 
 import GlobalStyles from 'style/global';
 
-function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>React Avançado</title>
-        <link rel="shortcut icon" href="/img/icon-192.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-      </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
-  );
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <title>React Avançado</title>
+      <link rel="shortcut icon" href="/img/icon-192.png" />
+      <link rel="apple-touch-icon" href="/img/icon-512.png" />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
