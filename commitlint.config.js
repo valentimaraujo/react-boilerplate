4d2584701c101890d04e447e2ca7f2a1985f94fe
Valentim Araújo <valentim@evodev.com.br>
2022-08-04 21:45:37 -0300

    fix: same changelog test

--- commitlint.config.js
+++ commitlint.config.js
@@ -10,6 +10,7 @@
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /^Bumps \[.+]\(.+\) from .+ to .+\.$/m.test(message)],
   rules: {
     'body-leading-blank': [1, 'always'],
     'body-max-line-length': [2, 'always', 100],
