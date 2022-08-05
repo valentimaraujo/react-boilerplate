5d08a7d3c02b82c8ab7d88a3de540d7ae647354d
Valentim Araújo <valentim@evodev.com.br>
2022-08-04 22:48:04 -0300

    build: implement PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
-};
+  optimization: {
+    mergeDuplicateChunks: true
+  }
+}
 
 module.exports = nextConfig;
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProduction,
+  }
+})
+
