c0f75b2b2adbb622850020f59f48098b5e7464b9
Valentim Araújo <valentim@evodev.com.br>
2022-08-08 09:16:10 -0300

    build: implemented protected routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,11 @@
 import type { GetServerSidePropsContext } from 'next';
-import { useEffect } from 'react';
 
 import protectedRoutes from '../core/utils/protected-routes';
 import Home, { IHome } from '../templates/home';
 
-const Index = ({ user }: IHome) => {
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
-
-  return (
-    <Home user={user} />
-  );
-};
+const Index = ({ user }: IHome) => (
+  <Home user={user} />
+);
 
 export default Index;
 export async function getServerSideProps(context: GetServerSidePropsContext) {
